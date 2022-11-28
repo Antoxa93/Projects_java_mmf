47d43088512ddb92ab86e735fa74b200e371aa2a
Anton Nemirovich <86725005+Antoxa93@users.noreply.github.com>
2022-11-28 12:22:55 +0200

    CW9_Nemyrovych

---
+++ Milk.java
@@ -0,0 +1,20 @@
+public class Milk extends Item{
+
+    int fat;
+
+    Item(){
+        this.type = "liquid";
+    }
+
+    Meal(float price, String name){
+        super(price, name, "liquid");
+    }
+
+    public int getFat(){
+        return fat;
+    }
+
+    public void setFat(int fat){
+        this.fat = fat;
+    }
+}
